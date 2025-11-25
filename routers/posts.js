2b1152cd8f0845ed8ca0838426c76df3c0fb876d
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-25 11:38:21 +0100

    Add CRUD operations to posts.js

--- routers/posts.js
+++ routers/posts.js
@@ -2,15 +2,33 @@ const express = require("express");
 const router = express.Router();
 
 // Index
+router.get("/", (req, res) => {
+	res.send("Lista dei post");
+});
 
 // Show
+router.get("/:id", (req, res) => {
+	res.send(`Visualizzazione post: ${req.params.id}`);
+});
 
 // Store
+router.post("/", (req, res) => {
+	res.send("Creato un nuovo post");
+});
 
 // Update
+router.put("/:id", (req, res) => {
+	res.send(`Modifica interamente post: ${req.params.id}`);
+});
 
 // Modify
+router.patch("/:id", (req, res) => {
+	res.send(`Modifica parzialemente post: ${req.params.id}`);
+});
 
 // Destroy
+router.delete("/:id", (req, res) => {
+	res.send(`Elimina il post ${req.params.id}`);
+});
 
 module.exports = router;
